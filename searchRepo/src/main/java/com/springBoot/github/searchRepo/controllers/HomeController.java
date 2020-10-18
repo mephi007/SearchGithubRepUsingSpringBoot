3af28bfb3a55d3bc6a5b0f0780049df72ab4bf26
mephi007 <roysumit949@gmail.com>
2020-10-18 13:20:14 +0530

    created Controller and checked if server is running or not by using simple get Method

---
+++ searchRepo/src/main/java/com/springBoot/github/searchRepo/controllers/HomeController.java
@@ -0,0 +1,26 @@
+package com.springBoot.github.searchRepo.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/searchRepo")
+public class HomeController {
+	
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "Hello";
+	}
+	
+	@PostMapping("/getByLanguage")
+	public List<Repos> getRepoByLanguage(@RequestBody String language){
+		
+	}
+	
+}
