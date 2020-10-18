a0c1bf6c3efb2a2aaa2d2744a729da847bdf795c
mephi007 <roysumit949@gmail.com>
2020-10-18 15:01:08 +0530

    adding JSON Object parsing dependency to pom.xml

--- searchRepo/src/main/java/com/springBoot/github/searchRepo/dao/DaoLayer.java
+++ searchRepo/src/main/java/com/springBoot/github/searchRepo/dao/DaoLayer.java
@@ -8,9 +8,11 @@ import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONObject;
 import org.springframework.stereotype.Repository;
 
 import com.springBoot.github.searchRepo.models.Repos;
+
 @Repository
 public class DaoLayer {
 	private static String REPO_URL = "https://api.github.com/search/issues?q=language:";
@@ -21,9 +23,10 @@ public class DaoLayer {
 		// TODO Auto-generated method stub
 		repos = new ArrayList();
 		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(REPO_URL+language)).build();
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(REPO_URL + language)).build();
 		HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
 		System.out.println("Reached DAO and hitted github api");
+		JSONObject jsonResponseBody = new JSONObject();
 		return repos;
 	}
 
