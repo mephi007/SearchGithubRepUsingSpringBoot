58b544bed5a0dece84218d8facc298795f035e46
mephi007 <roysumit949@gmail.com>
2020-10-18 13:04:43 +0530

    created SpringBoot boiler paltes using Spring initializr

---
+++ searchRepo/src/main/java/com/springBoot/github/searchRepo/SearchRepoApplication.java
@@ -0,0 +1,13 @@
+package com.springBoot.github.searchRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchRepoApplication.class, args);
+	}
+
+}
---
+++ searchRepo/src/test/java/com/springBoot/github/searchRepo/SearchRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.github.searchRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
