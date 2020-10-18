cac57330e6108bfb2de75d7021a16e92717dd0ed
mephi007 <roysumit949@gmail.com>
2020-10-18 13:28:46 +0530

    created model of Repos

---
+++ searchRepo/src/main/java/com/springBoot/github/searchRepo/models/Repos.java
@@ -0,0 +1,51 @@
+package com.springBoot.github.searchRepo.models;
+
+
+public class Repos {
+	private String id;
+	private String title;
+	private String url;
+	private String user;
+	
+	public Repos(String id, String title, String url, String user) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.url = url;
+		this.user = user;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+	
+	
+}
